e5c8c769eaacf81423517d2d0e1edfbc23d724f4
Jessica Gladstone <jessgladtobe@hotmail.com>
2019-12-05 19:01:47 -0500

    set up express server

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+
+const app = express(); 
+
+app.use(fileUpload()); 
+
+
+app.listen(5000, () => console.log('Server Active...'));
