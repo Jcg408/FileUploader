f17257af216c174e9fc628892fc5223c2f3eca1e
Jcg408 <jcg321@comcast.net>
2019-12-07 11:41:34 -0500

    import FileUploader to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import FileUploader from './components/FileUpload'
 import './App.css';
 
 const App = () => (
@@ -6,6 +7,7 @@ const App = () => (
         <h4 className='display-4 text-center mb-4'>
             <i className='fa fa-arrow-up' /> File Uploader
         </h4>
+        <FileUploader/>
     </div>
 );
 export default App;
