800398fab80346753dee0be7a39be52353f9ac05
Jcg408 <jcg321@comcast.net>
2019-12-07 13:12:56 -0500

    add hooks, and events to FileUploads.js

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,20 +1,40 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+
 
 const FileUpload = () => {
+    const [file, setFile] = useState(' ');
+    const [filename, setFilename] = useState('Choose File');
+
+    const handleChange = event => {
+        setFile(event.target.files[0]);
+        setFilename(event.target.files[0].name);
+    };
+
+    const handleSubmit = async  event => {
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append('file', file);
+       
+    };
     return (
         <Fragment>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className='custom-file mb-4'>
                     <input
                         type='file'
                         className='custom-file-input'
                         id='customFile'
+                        onChange={handleChange}
                     />
                     <label className='custom-file-label' htmlFor='customFile'>
-                        Choose file
+                        {filename}
                     </label>
                 </div>
-               <input type="submit" value="Upload" className="btn btn-primary btn-block mt-4" />
+                <input
+                    type='submit'
+                    value='Upload'
+                    className='btn btn-primary btn-block mt-4'
+                />
             </form>
         </Fragment>
     );
